e5423ab003f0b7c25c70067d56ec2e1e6338ccd1
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-05-07 21:41:13 -0400

    created function that captures relevant data and stores it within an array for future access

--- js/script.js
+++ js/script.js
@@ -1,26 +1,52 @@
 // VARIABLES
-const API = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=4PDBxeBJbs8UKxXS7f3dzh6lmYb6hNulqgtrZMq4&query=";  // this references to the Edamame API. Add the ingredient to the end to access the data
+const API =
+  "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=4PDBxeBJbs8UKxXS7f3dzh6lmYb6hNulqgtrZMq4&query="; // this references to the Edamame API. Add the ingredient to the end to access the data
+
+
+const foodArray = [];
 
 // EVENT REFERENCES
-const $btnSearch = $('#btnSearch');
-const $btnAdd = $('#btnAdd');
+const $btnSearch = $("#btnSearch");
+const $btnAdd = $("#btnAdd");
 const $userInput = $('#userInput[type="text"]');
 // EVENT LISTENERS
-$btnSearch.on('click', retreiveMacro);
+$btnSearch.on("click", retreiveMacro);
 // $btnAdd.on('click', addToList);
 // FUNCTIONS
 
 function retreiveMacro(evt) {
-    evt.preventDefault();
-    const userInput = $userInput.val();
-    $.ajax(API + userInput).then(function(data){
-        const firstResult = data.foods[0];
-        console.log(firstResult) // this code logs the first
-    }, function(error) {
-        console.log('Unexpected Error has occured, see below')
-        console.log(error)
+  evt.preventDefault();
+  const userInput = $userInput.val();
+  $.ajax(API + userInput).then(
+    function (data) {
+           const foodArray = [data.foods]; // this line stores every result into an array
+            console.log(foodArray);
+        
+    //   const firstResult = data.foods[0];
+    //   console.log(firstResult); // this code logs the first
+    },
+    function (error) {
+      console.log("Unexpected Error has occured, see below");
+      console.log(error);
     }
-    
-    )
+  );
 }
 
+
+// function to retrieve food results and turn them into array
+
+
+
+
+
+/* 
+NUTRITION CODES:
+
+PROTEIN: 1003
+FAT: 1004
+CARBOHYDRATE:1005
+CALORIES (IN KCAL): 1008
+
+
+
+*/
